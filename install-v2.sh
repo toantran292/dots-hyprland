5f8c1d369e100c9efaa49547a8da38c945af33ad
toantran292 <tranthaitoan998@gmail.com>
2024-06-03 10:48:09 +0700

    init

--- install-v2.sh
+++ install-v2.sh
@@ -7,314 +7,314 @@ source .install/includes/functions.sh
 source .install/includes/options.sh
 
 #####################################################################################
-# if ! command -v pacman >/dev/null 2>&1; then
-#   echo -e "${RED}[$0]: pacman not found, it seems that the system is not ArchLinux or Arch-based distros. Aborting...${NONE}"
-#   exit 1
-# fi
-# prevent_sudo_or_root
-# startask() {
-#   echo -e "${BLUE}"
-#   echo "[$0]: Hi there! Before we start:"
-#   echo "This script 1. only works for ArchLinux and Arch-based distros."
-#   echo "            2. does not handle system-level/hardware stuff like Nvidia drivers"
+if ! command -v pacman >/dev/null 2>&1; then
+  echo -e "${RED}[$0]: pacman not found, it seems that the system is not ArchLinux or Arch-based distros. Aborting...${NONE}"
+  exit 1
+fi
+prevent_sudo_or_root
+startask() {
+  echo -e "${BLUE}"
+  echo "[$0]: Hi there! Before we start:"
+  echo "This script 1. only works for ArchLinux and Arch-based distros."
+  echo "            2. does not handle system-level/hardware stuff like Nvidia drivers"
 
-#   if gum confirm "Please CONFIRM that you HAVE ALREADY BACKED UP \"$HOME/.config/\" and \"$HOME/.local/\" folders!"; then
-#     sleep 0
-#   else
-#     echo "Received \"$p\", aborting..."
-#     exit 1
-#   fi
+  if gum confirm "Please CONFIRM that you HAVE ALREADY BACKED UP \"$HOME/.config/\" and \"$HOME/.local/\" folders!"; then
+    sleep 0
+  else
+    echo "Received \"$p\", aborting..."
+    exit 1
+  fi
 
-#   echo -e "${NONE}"
-#   echo "Do you want to confirm every time before a command executes?"
-#   p=$(gum choose "Yes, ask me before executing each of them." "No, just execute them automatically." "Abort.")
-#   case $p in
-#   "No, just execute them automatically.") ask=false ;;
-#   "Abort.") exit 1 ;;
-#   *) ask=true ;;
-#   esac
-# }
+  echo -e "${NONE}"
+  echo "Do you want to confirm every time before a command executes?"
+  p=$(gum choose "Yes, ask me before executing each of them." "No, just execute them automatically." "Abort.")
+  case $p in
+  "No, just execute them automatically.") ask=false ;;
+  "Abort.") exit 1 ;;
+  *) ask=true ;;
+  esac
+}
 
-# case $ask in
-# false) sleep 0 ;;
-# *) startask ;;
-# esac
+case $ask in
+false) sleep 0 ;;
+*) startask ;;
+esac
 
 set -e
 #####################################################################################
 echo -e "${LBLUE}[$0]: 1. Get packages and setup user groups/services${NONE}"
-# case $SKIP_SYSUPDATE in
-# true) sleep 0 ;;
-# *) v sudo pacman -Syu ;;
-# esac
+case $SKIP_SYSUPDATE in
+true) sleep 0 ;;
+*) v sudo pacman -Syu ;;
+esac
 
-# remove_bashcomments_emptylines ${DEPLISTFILE} ./cache/dependencies_stripped.conf
-# readarray -t pkglist <./cache/dependencies_stripped.conf
+remove_bashcomments_emptylines ${DEPLISTFILE} ./cache/dependencies_stripped.conf
+readarray -t pkglist <./cache/dependencies_stripped.conf
 
-# if ! command -v yay >/dev/null 2>&1; then
-#   if ! command -v paru >/dev/null 2>&1; then
-#     echo -e "${YELLOW}[$0]: \"yay\" not found.${NONE}"
-#     showfun install-yay
-#     v install-yay
-#     AUR_HELPER=yay
-#   else
-#     echo -e "${YELLOW}"
-#     echo -e "[$0]: \"yay\" not found, but \"paru\" found."
-#     echo -e "It is not recommended to use \"paru\" as warned in Hyprland Wiki:"
-#     echo -e "    \"If you are using the AUR (hyprland-git) package, you will need to cleanbuild to update the package. Paru has been problematic with updating before, use Yay.\""
-#     echo -e "Reference: https://wiki.hyprland.org/FAQ/#how-do-i-update"
-#     echo -e "${NONE}"
-#     if $ask; then
-#       printf "Install \"yay\"?\n"
-#       p=$(gum choose "Yes, install \"yay\" for me first." "No, use \"paru\" at my own risk." "Abort.")
-#       sleep 2
-#       case $p in
-#       "No, use \"paru\" at my own risk.") AUR_HELPER=paru ;;
-#       "Abort.")
-#         echo -e "${LBLUE}Alright, aborting...${NONE}"
-#         exit 1
-#         ;;
-#       "Yes, install \"yay\" for me first.")
-#         v paru -S --needed --noconfirm yay-bin
-#         AUR_HELPER=yay
-#         ;;
-#       esac
-#     else
-#       AUR_HELPER=paru
-#     fi
-#   fi
-# else
-#   AUR_HELPER=yay
-# fi
+if ! command -v yay >/dev/null 2>&1; then
+  if ! command -v paru >/dev/null 2>&1; then
+    echo -e "${YELLOW}[$0]: \"yay\" not found.${NONE}"
+    showfun install-yay
+    v install-yay
+    AUR_HELPER=yay
+  else
+    echo -e "${YELLOW}"
+    echo -e "[$0]: \"yay\" not found, but \"paru\" found."
+    echo -e "It is not recommended to use \"paru\" as warned in Hyprland Wiki:"
+    echo -e "    \"If you are using the AUR (hyprland-git) package, you will need to cleanbuild to update the package. Paru has been problematic with updating before, use Yay.\""
+    echo -e "Reference: https://wiki.hyprland.org/FAQ/#how-do-i-update"
+    echo -e "${NONE}"
+    if $ask; then
+      printf "Install \"yay\"?\n"
+      p=$(gum choose "Yes, install \"yay\" for me first." "No, use \"paru\" at my own risk." "Abort.")
+      sleep 2
+      case $p in
+      "No, use \"paru\" at my own risk.") AUR_HELPER=paru ;;
+      "Abort.")
+        echo -e "${LBLUE}Alright, aborting...${NONE}"
+        exit 1
+        ;;
+      "Yes, install \"yay\" for me first.")
+        v paru -S --needed --noconfirm yay-bin
+        AUR_HELPER=yay
+        ;;
+      esac
+    else
+      AUR_HELPER=paru
+    fi
+  fi
+else
+  AUR_HELPER=yay
+fi
 
-# if $ask; then
-#   # execute per element of the array $pkglist
-#   for i in "${pkglist[@]}"; do v $AUR_HELPER -S --needed $i; done
-# else
-#   # execute for all elements of the array $pkglist in one line
-#   v $AUR_HELPER -S --needed --noconfirm ${pkglist[*]}
-# fi
+if $ask; then
+  # execute per element of the array $pkglist
+  for i in "${pkglist[@]}"; do v $AUR_HELPER -S --needed $i; done
+else
+  # execute for all elements of the array $pkglist in one line
+  v $AUR_HELPER -S --needed --noconfirm ${pkglist[*]}
+fi
 
-# case $SKIP_HYPR_AUR in
-# true) sleep 0 ;;
-# *)
-#   if $ask; then
-#     v $AUR_HELPER -S --answerclean=a hyprland-git
-#   else
-#     v $AUR_HELPER -S --answerclean=a --noconfirm hyprland-git
-#   fi
-#   ;;
-# esac
+case $SKIP_HYPR_AUR in
+true) sleep 0 ;;
+*)
+  if $ask; then
+    v $AUR_HELPER -S --answerclean=a hyprland-git
+  else
+    v $AUR_HELPER -S --answerclean=a --noconfirm hyprland-git
+  fi
+  ;;
+esac
 
-# pymyc=(python-materialyoucolor-git gradience-git python-libsass python-material-color-utilities)
-# case $SKIP_PYMYC_AUR in
-# true) sleep 0 ;;
-# *)
-#   if $ask; then
-#     v $AUR_HELPER -S --answerclean=a ${pymyc[@]}
-#   else
-#     v $AUR_HELPER -S --answerclean=a --noconfirm ${pymyc[@]}
-#   fi
-#   ;;
-# esac
+pymyc=(python-materialyoucolor-git gradience-git python-libsass python-material-color-utilities)
+case $SKIP_PYMYC_AUR in
+true) sleep 0 ;;
+*)
+  if $ask; then
+    v $AUR_HELPER -S --answerclean=a ${pymyc[@]}
+  else
+    v $AUR_HELPER -S --answerclean=a --noconfirm ${pymyc[@]}
+  fi
+  ;;
+esac
 
-# if pacman -Qs ^plasma-browser-integration$; then SKIP_PLASMAINTG=true; fi
+if pacman -Qs ^plasma-browser-integration$; then SKIP_PLASMAINTG=true; fi
 
-# case $SKIP_PLASMAINTG in
-# true) sleep 0 ;;
-# *)
-#   if $ask; then
-#     echo -e "${LBLUE}"
-#     echo -e "[$0]: 2. Install \"plasma-browser-integration\""
-#     echo -e "NOTE: The size of \"plasma-browser-integration\" is about 250 MiB."
-#     echo -e "It is needed if you want playtime of media in Firefox to be shown on the music controls widget."
-#     echo -e "${NONE}"
-#     if gum confirm "Install it?"; then
-#       p=y
-#     fi
-#   else
-#     p=y
-#   fi
-#   case $p in
-#   y) x sudo pacman -S --needed --noconfirm plasma-browser-integration ;;
-#   *) echo "Ok, won't install" ;;
-#   esac
-#   ;;
-# esac
+case $SKIP_PLASMAINTG in
+true) sleep 0 ;;
+*)
+  if $ask; then
+    echo -e "${LBLUE}"
+    echo -e "[$0]: 2. Install \"plasma-browser-integration\""
+    echo -e "NOTE: The size of \"plasma-browser-integration\" is about 250 MiB."
+    echo -e "It is needed if you want playtime of media in Firefox to be shown on the music controls widget."
+    echo -e "${NONE}"
+    if gum confirm "Install it?"; then
+      p=y
+    fi
+  else
+    p=y
+  fi
+  case $p in
+  y) x sudo pacman -S --needed --noconfirm plasma-browser-integration ;;
+  *) echo "Ok, won't install" ;;
+  esac
+  ;;
+esac
 
-# v sudo usermod -aG video,i2c,input "$(whoami)"
-# v bash -c "echo i2c-dev | sudo tee /etc/modules-load.d/i2c-dev.conf"
-# v systemctl --user enable ydotool --now
+v sudo usermod -aG video,i2c,input "$(whoami)"
+v bash -c "echo i2c-dev | sudo tee /etc/modules-load.d/i2c-dev.conf"
+v systemctl --user enable ydotool --now
 
 #####################################################################################
 echo -e "${LBLUE}[$0]: 2. Installing parts from source repo${NONE}"
 sleep 1
 
-# case $SKIP_AGS in
-# true) sleep 0 ;;
-# *)
-#   if command -v ags >/dev/null 2>&1; then
-#     echo -e "${YELLOW}[$0]: Command \"ags\" already exists, no need to install.${NONE}"
-#     echo -e "${LBLUE}You can reinstall it in order to update to the latest version anyway.${NONE}"
-#     ask_ags=$ask
-#   else
-#     ask_ags=true
-#   fi
-#   if $ask_ags; then
-#     showfun install-ags
-#     v install-ags
-#   fi
-#   ;;
-# esac
+case $SKIP_AGS in
+true) sleep 0 ;;
+*)
+  if command -v ags >/dev/null 2>&1; then
+    echo -e "${YELLOW}[$0]: Command \"ags\" already exists, no need to install.${NONE}"
+    echo -e "${LBLUE}You can reinstall it in order to update to the latest version anyway.${NONE}"
+    ask_ags=$ask
+  else
+    ask_ags=true
+  fi
+  if $ask_ags; then
+    showfun install-ags
+    v install-ags
+  fi
+  ;;
+esac
 
-# if $(fc-list | grep -q Rubik); then
-#   echo -e "${YELLOW}[$0]: Font \"Rubik\" already exists, no need to install.${NONE}"
-#   echo -e "${LBLUE}You can reinstall it in order to update to the latest version anyway.${NONE}"
-#   ask_Rubik=$ask
-# else
-#   ask_Rubik=true
-# fi
-# if $ask_Rubik; then
-#   showfun install-Rubik
-#   v install-Rubik
-# fi
+if $(fc-list | grep -q Rubik); then
+  echo -e "${YELLOW}[$0]: Font \"Rubik\" already exists, no need to install.${NONE}"
+  echo -e "${LBLUE}You can reinstall it in order to update to the latest version anyway.${NONE}"
+  ask_Rubik=$ask
+else
+  ask_Rubik=true
+fi
+if $ask_Rubik; then
+  showfun install-Rubik
+  v install-Rubik
+fi
 
-# if $(fc-list | grep -q Gabarito); then
-#   echo -e "${YELLOW}[$0]: Font \"Gabarito\" already exists, no need to install.${NONE}"
-#   echo -e "${LBLUE}You can reinstall it in order to update to the latest version anyway.${NONE}"
-#   ask_Gabarito=$ask
-# else
-#   ask_Gabarito=true
-# fi
-# if $ask_Gabarito; then
-#   showfun install-Gabarito
-#   v install-Gabarito
-# fi
+if $(fc-list | grep -q Gabarito); then
+  echo -e "${YELLOW}[$0]: Font \"Gabarito\" already exists, no need to install.${NONE}"
+  echo -e "${LBLUE}You can reinstall it in order to update to the latest version anyway.${NONE}"
+  ask_Gabarito=$ask
+else
+  ask_Gabarito=true
+fi
+if $ask_Gabarito; then
+  showfun install-Gabarito
+  v install-Gabarito
+fi
 
-# if $(test -d /usr/local/share/icons/OneUI); then
-#   echo -e "${YELLOW}[$0]: Icon pack \"OneUI\" already exists, no need to install.${NONE}"
-#   echo -e "${LBLUE}You can reinstall it in order to update to the latest version anyway.${NONE}"
-#   ask_OneUI=$ask
-# else
-#   ask_OneUI=true
-# fi
-# if $ask_OneUI; then
-#   showfun install-OneUI
-#   v install-OneUI
-# fi
+if $(test -d /usr/local/share/icons/OneUI); then
+  echo -e "${YELLOW}[$0]: Icon pack \"OneUI\" already exists, no need to install.${NONE}"
+  echo -e "${LBLUE}You can reinstall it in order to update to the latest version anyway.${NONE}"
+  ask_OneUI=$ask
+else
+  ask_OneUI=true
+fi
+if $ask_OneUI; then
+  showfun install-OneUI
+  v install-OneUI
+fi
 
-# if $(test -d /usr/local/share/icons/Bibata-Modern-Classic); then
-#   echo -e "${YELLOW}[$0]: Cursor theme \"Bibata-Modern-Classic\" already exists, no need to install.${NONE}"
-#   echo -e "${LBLUE}You can reinstall it in order to update to the latest version anyway.${NONE}"
-#   ask_bibata=$ask
-# else
-#   ask_bibata=true
-# fi
-# if $ask_bibata; then
-#   showfun install-bibata
-#   v install-bibata
-# fi
+if $(test -d /usr/local/share/icons/Bibata-Modern-Classic); then
+  echo -e "${YELLOW}[$0]: Cursor theme \"Bibata-Modern-Classic\" already exists, no need to install.${NONE}"
+  echo -e "${LBLUE}You can reinstall it in order to update to the latest version anyway.${NONE}"
+  ask_bibata=$ask
+else
+  ask_bibata=true
+fi
+if $ask_bibata; then
+  showfun install-bibata
+  v install-bibata
+fi
 
-# if command -v LaTeX >/dev/null 2>&1; then
-#   echo -e "${YELLOW}[$0]: Program \"LaTeX\" already exists, no need to install.${NONE}"
-#   echo -e "${LBLUE}You can reinstall it in order to update to the latest version anyway.${NONE}"
-#   ask_MicroTeX=$ask
-# else
-#   ask_MicroTeX=true
-# fi
-# if $ask_MicroTeX; then
-#   showfun install-MicroTeX
-#   v install-MicroTeX
-# fi
+if command -v LaTeX >/dev/null 2>&1; then
+  echo -e "${YELLOW}[$0]: Program \"LaTeX\" already exists, no need to install.${NONE}"
+  echo -e "${LBLUE}You can reinstall it in order to update to the latest version anyway.${NONE}"
+  ask_MicroTeX=$ask
+else
+  ask_MicroTeX=true
+fi
+if $ask_MicroTeX; then
+  showfun install-MicroTeX
+  v install-MicroTeX
+fi
 #####################################################################################
 echo -e "${LBLUE}[$0]: 3. Copying + Configuring${NONE}"
 
-# # In case some folders does not exists
-# v mkdir -p "$HOME"/.{config,cache,local/{bin,share}}
+# In case some folders does not exists
+v mkdir -p "$HOME"/.{config,cache,local/{bin,share}}
 
-# # `--delete' for rsync to make sure that
-# # original dotfiles and new ones in the SAME DIRECTORY
-# # (eg. in ~/.config/hypr) won't be mixed together
+# `--delete' for rsync to make sure that
+# original dotfiles and new ones in the SAME DIRECTORY
+# (eg. in ~/.config/hypr) won't be mixed together
 
-# # MISC (For .config/* but not AGS, not Fish, not Hyprland)
-# case $SKIP_MISCCONF in
-# true) sleep 0 ;;
-# *)
-#   for i in $(find .config/ -mindepth 1 -maxdepth 1 ! -name 'ags' ! -name 'fish' ! -name 'hypr' -exec basename {} \;); do
-#     i=".config/$i"
-#     echo "[$0]: Found target: $i"
-#     if [ -d "$i" ]; then
-#       v rsync -av --delete "$i/" "$HOME/$i/"
-#     elif [ -f "$i" ]; then
-#       v rsync -av "$i" "$HOME/$i"
-#     fi
-#   done
-#   ;;
-# esac
+# MISC (For .config/* but not AGS, not Fish, not Hyprland)
+case $SKIP_MISCCONF in
+true) sleep 0 ;;
+*)
+  for i in $(find .config/ -mindepth 1 -maxdepth 1 ! -name 'ags' ! -name 'fish' ! -name 'hypr' -exec basename {} \;); do
+    i=".config/$i"
+    echo "[$0]: Found target: $i"
+    if [ -d "$i" ]; then
+      v rsync -av --delete "$i/" "$HOME/$i/"
+    elif [ -f "$i" ]; then
+      v rsync -av "$i" "$HOME/$i"
+    fi
+  done
+  ;;
+esac
 
-# case $SKIP_FISH in
-# true) sleep 0 ;;
-# *)
-#   v rsync -av --delete .config/fish/ "$HOME"/.config/fish/
-#   ;;
-# esac
+case $SKIP_FISH in
+true) sleep 0 ;;
+*)
+  v rsync -av --delete .config/fish/ "$HOME"/.config/fish/
+  ;;
+esac
 
-# # For AGS
-# case $SKIP_AGS in
-# true) sleep 0 ;;
-# *)
-#   v rsync -av --delete --exclude '/user_options.js' .config/ags/ "$HOME"/.config/ags/
-#   t="$HOME/.config/ags/user_options.js"
-#   if [ -f $t ]; then
-#     echo -e "${BLUE}[$0]: \"$t\" already exists.${NONE}"
-#     # v cp -f .config/ags/user_options.js $t.new
-#     existed_ags_opt=y
-#   else
-#     echo -e "${YELLOW}[$0]: \"$t\" does not exist yet.${NONE}"
-#     v cp .config/ags/user_options.js $t
-#     existed_ags_opt=n
-#   fi
-#   ;;
-# esac
+# For AGS
+case $SKIP_AGS in
+true) sleep 0 ;;
+*)
+  v rsync -av --delete --exclude '/user_options.js' .config/ags/ "$HOME"/.config/ags/
+  t="$HOME/.config/ags/user_options.js"
+  if [ -f $t ]; then
+    echo -e "${BLUE}[$0]: \"$t\" already exists.${NONE}"
+    # v cp -f .config/ags/user_options.js $t.new
+    existed_ags_opt=y
+  else
+    echo -e "${YELLOW}[$0]: \"$t\" does not exist yet.${NONE}"
+    v cp .config/ags/user_options.js $t
+    existed_ags_opt=n
+  fi
+  ;;
+esac
 
-# # For Hyprland
-# case $SKIP_HYPRLAND in
-# true) sleep 0 ;;
-# *)
-#   v rsync -av --delete --exclude '/custom' --exclude '/hyprland.conf' .config/hypr/ "$HOME"/.config/hypr/
-#   t="$HOME/.config/hypr/hyprland.conf"
-#   if [ -f $t ]; then
-#     echo -e "${BLUE}[$0]: \"$t\" already exists.${NONE}"
-#     v cp -f .config/hypr/hyprland.conf $t.new
-#     existed_hypr_conf=y
-#   else
-#     echo -e "${YELLOW}[$0]: \"$t\" does not exist yet.${NONE}"
-#     v cp .config/hypr/hyprland.conf $t
-#     existed_hypr_conf=n
-#   fi
-#   t="$HOME/.config/hypr/custom"
-#   if [ -d $t ]; then
-#     echo -e "${BLUE}[$0]: \"$t\" already exists, will not do anything.${NONE}"
-#   else
-#     echo -e "${YELLOW}[$0]: \"$t\" does not exist yet.${NONE}"
-#     v rsync -av --delete .config/hypr/custom/ $t/
-#   fi
-#   ;;
-# esac
+# For Hyprland
+case $SKIP_HYPRLAND in
+true) sleep 0 ;;
+*)
+  v rsync -av --delete --exclude '/custom' --exclude '/hyprland.conf' .config/hypr/ "$HOME"/.config/hypr/
+  t="$HOME/.config/hypr/hyprland.conf"
+  if [ -f $t ]; then
+    echo -e "${BLUE}[$0]: \"$t\" already exists.${NONE}"
+    v cp -f .config/hypr/hyprland.conf $t.new
+    existed_hypr_conf=y
+  else
+    echo -e "${YELLOW}[$0]: \"$t\" does not exist yet.${NONE}"
+    v cp .config/hypr/hyprland.conf $t
+    existed_hypr_conf=n
+  fi
+  t="$HOME/.config/hypr/custom"
+  if [ -d $t ]; then
+    echo -e "${BLUE}[$0]: \"$t\" already exists, will not do anything.${NONE}"
+  else
+    echo -e "${YELLOW}[$0]: \"$t\" does not exist yet.${NONE}"
+    v rsync -av --delete .config/hypr/custom/ $t/
+  fi
+  ;;
+esac
 
-# # some foldes (eg. .local/bin) should be processed separately to avoid `--delete' for rsync,
-# # since the files here come from different places, not only about one program.
-# v rsync -av ".local/bin/" "$HOME/.local/bin/"
+# some foldes (eg. .local/bin) should be processed separately to avoid `--delete' for rsync,
+# since the files here come from different places, not only about one program.
+v rsync -av ".local/bin/" "$HOME/.local/bin/"
 
-# # Dark mode by default
-# v gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+# Dark mode by default
+v gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
-# # Prevent hyprland from not fully loaded
-# sleep 1
-# try hyprctl reload
+# Prevent hyprland from not fully loaded
+sleep 1
+try hyprctl reload
 
-# existed_zsh_conf=n
-# grep -q 'source ~/.config/zshrc.d/dots-hyprland.zsh' ~/.zshrc && existed_zsh_conf=y
+existed_zsh_conf=n
+grep -q 'source ~/.config/zshrc.d/dots-hyprland.zsh' ~/.zshrc && existed_zsh_conf=y
 
 #####################################################################################
 echo -e "${LBLUE}"
